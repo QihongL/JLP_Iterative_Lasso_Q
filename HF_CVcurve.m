ab307f99a76270df8422251164c3bb01c0068a11
QihongL <lvqihong1992@gmail.com>
2014-08-05 11:17:48 -0500

    Plotting CV curves for HFcvglmnet

---
+++ HF_CVcurve.m
@@ -0,0 +1,48 @@
+%% Generate cross validation curves for HFcvglmnet
+% Results for iterative lasso using HFcvglmnet has to be loaded first!
+clear; close all;
+
+%% Load the data
+load('JLP_HF_TrueFaces.mat')
+
+%% Plot the CV curve for subject i, iteration j
+% specify the parameters
+subNum = 1;
+numIter = 1;
+% Get data 
+hitRate = result(subNum).HF_tunning_lambda{numIter}.hitrate;
+falseRate = result(subNum).HF_tunning_lambda{numIter}.falserate;
+difference = result(subNum).HF_tunning_lambda{numIter}.difference;
+
+figure(1)
+HFcvglmnetPlot( hitRate, falseRate, difference)
+
+
+
+%% Plot the CV curve for iteration j for all subjects
+% specify the paratemer
+numIter = 1;
+
+% loop over all subjects
+for subNum = 1:10 
+    if subNum == 1 
+        % for the 1st subject, create variables
+        hitRate = result(subNum).HF_tunning_lambda{numIter}.hitrate;
+        falseRate = result(subNum).HF_tunning_lambda{numIter}.falserate;
+        difference = result(subNum).HF_tunning_lambda{numIter}.difference;
+    else
+        % for the rest of subjects, concatnate the results with the
+        % previous results
+        tempH = result(subNum).HF_tunning_lambda{numIter}.hitrate;
+        tempF = result(subNum).HF_tunning_lambda{numIter}.falserate;
+        tempD = result(subNum).HF_tunning_lambda{numIter}.difference;
+        hitRate = [ hitRate; tempH ];
+        falseRate= [falseRate; tempF];
+        difference = [difference; tempD];
+    end
+end
+
+figure(2)
+HFcvglmnetPlot( hitRate, falseRate, difference)
+
+

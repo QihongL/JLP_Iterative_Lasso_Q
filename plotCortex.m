9a3554b8663ce9a94479c2f85f5e2ebcb435daef
QihongL <lvqihong1992@gmail.com>
2014-08-14 11:48:01 -0500

    Plot the cortex
    Some minor changes

--- plotCortex.m
+++ plotCortex.m
@@ -1,9 +1,11 @@
-% Generate NIFTI
+%% Generate NIFTI
 clear;
 
-
+% load the coordinates
 data = load_nii('ribbon01_funcRes+orig.nii.gz');
-
+% load IJK
 load('/Users/lcnl/Documents/MATLAB/JLP/data/jlp01.mat', 'IJK');
 
-writeNIFTI('testing.nii',[64,64,30],IJK,data.hdr, 'fix');
+
+%% write to nifti file
+writeNIFTI('testing.nii',[64,64,30],IJK, 1, data.hdr, 'fix');

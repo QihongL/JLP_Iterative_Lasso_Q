8612a1868d1f32fa8eef5cb3d396f3596999898d
QihongL <lvqihong1992@gmail.com>
2014-08-21 10:49:58 -0500

    Get weights for all subj
    problem: lasso could select nothing
    so some final models do not exist

---
+++ finalModel_all.m
@@ -0,0 +1,96 @@
+%% Get final models - only exclude the last in-sig. iter.
+% - Prepration Winner take all analysis 
+% Only exclude the last insignificant iteration, in order to make sure
+% every subject has at least something to be analyzed. 
+
+clear; 
+
+%% Set the parameters
+type = 'ERR';
+label = 'TrueFaces';
+
+k = 10;
+STOPPING_RULE = 1;
+
+%% Load the result
+load(['JLP_' type '_' label '.mat']);
+
+for subNum = 1:10
+    disp(['Subject : ' num2str(subNum)])
+    %% Load the data
+    [X,metadata] = loadMRIData('jlp',subNum);
+
+    % Get CV inidices for testing and training set
+    CVBLOCKS = metadata(subNum).CVBLOCKS;
+    % Get the metadata for the row labels: 
+    Y = metadata(subNum).(label);
+
+
+    %% prep 1
+
+    % Only exclude the last iteration
+    iterNum = size(result(subNum).used,2) - STOPPING_RULE;
+
+%     %  resource preallocation
+%     accuracy = zeros(1,10);
+%     hitRate = zeros(1,10);
+%     falseRate = zeros(1,10);
+
+    textprogressbar(['Fitting final model: '  ])
+    for cv = 1:10
+        textprogressbar(cv * 10);
+        %% prep 2
+
+        % Get indices for all voxels that were being used
+        voxel_used = result(subNum).used{iterNum}(cv,:);
+        
+        
+        % Fit the final model only when solution exists
+        if sum(voxel_used) >= 1
+
+            % Get data for final model
+            Xfinal = X(:,voxel_used);
+
+            %% Get CV set & training set
+            FINAL_HOLDOUT = CVBLOCKS(:,cv);
+            Xtrain = Xfinal(~FINAL_HOLDOUT,:);
+            Xtest = Xfinal(FINAL_HOLDOUT,:);
+            Ytrain = Y(~FINAL_HOLDOUT);  
+            Ytest = Y(FINAL_HOLDOUT); 
+
+            temp = CVBLOCKS(~FINAL_HOLDOUT,(1:k)~=cv); 
+            fold_id = sum(bsxfun(@times,double(temp),1:9),2);   
+
+            %% Ridge
+
+            % Use ridge / tune lambda 
+            opts = glmnetSet(); 
+            opts.alpha = 1;   
+            fitObj_cvFinal = cvglmnet (Xtrain ,Ytrain, 'binomial',opts , 'class', k - 1, fold_id');
+            % Pick the best lambda 
+            opts.lambda = fitObj_cvFinal.lambda_min;
+
+            % Final fit using ridge
+            fitObj_Final = glmnet(Xtrain, Ytrain, 'binomial', opts);
+            fitStore(subNum).cv(cv) = fitObj_Final;
+
+
+
+    %         %% Predict
+    %         
+    %         % Compute prediction 
+    %         prediction = bsxfun(@plus, Xtest*fitObj_Final.beta, fitObj_Final.a0) > 0;
+    % 
+    %         % Compute accuracy / hit rate / false alarm rate 
+    %         accuracy(cv) = mean(Ytest == prediction);
+    %         hitRate(cv) = sum(prediction & Ytest) / sum(Ytest);
+    %         falseRate(cv) = sum(prediction & ~Ytest) / sum(Ytest);
+        end
+
+
+    end
+%     difference = hitRate - falseRate;
+    textprogressbar(['Done!'])
+
+end
+

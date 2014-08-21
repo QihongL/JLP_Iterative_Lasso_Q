6c9d7a37e4310cb8f3409e296d3c5b3086b9f283
QihongL <lvqihong1992@gmail.com>
2014-08-21 10:49:16 -0500

    Main script for running iterative lasso
    Nothing has changed

--- Iterative_Lasso_JLP.m
+++ Iterative_Lasso_JLP.m
@@ -4,7 +4,6 @@ clear; close all; clc;
 
 %% Run iterative lasso on a single subject
 
-
 % Specify the subject number
 subNum = 1;
 
@@ -22,65 +21,65 @@ Y = metadata(subNum).TrueFaces;
 
 
 
-%% Run all subjects with all labels, and save the results 
-
-loop over 3 labels
-for i = 1 : 3
-
-    if i == 1
-        label = 'TrueFaces';
-    elseif i == 2
-        label = 'TruePlaces';
-    elseif i == 3
-        label = 'TrueThings';
-    end
-
-    
-     
-    % Loop over 10 subjects
-    for subNum = 1:10
-        disp(['Calculating: ' label ' | Subject: ' num2str(subNum) '...' ])
-
-        %% prep
-        [X,metadata] = loadMRIData('jlp',subNum);
-        CVBLOCKS = metadata(subNum).CVBLOCKS;
-        Y = metadata(subNum).(label);
-
-
-        %% Choose the version of Iterative Lasso
-        [ hit, final, lasso, ridge, USED, fitStore ] = IterLasso(X,Y,CVBLOCKS,2);
-%         [ hit, final, lasso, ridge, USED, HF, fitStore ] = HFiterLasso(X,Y,CVBLOCKS,2);
-
-
-        %% Get results, and save them
-        result(subNum).finalAccuracy = final.accuracy;
-        result(subNum).hitAll = hit.all;
-        result(subNum).hitCurrent = hit.current;
-        result(subNum).lasso_accuracy = lasso.accuracy;
-        result(subNum).lasso_sig = lasso.sig;
-        result(subNum).ridgeAccuracy = ridge.accuracy;
-        result(subNum).used = USED;
-        result(subNum).lasso_hitRate = hit.hitRate;
-        result(subNum).lasso_falseRate = hit.falseRate;
-        result(subNum).lasso_difference = hit.diffHF;
-        result(subNum).lasso_difference = hit.diffHF;
-        result(subNum).final_hitRate = final.hitrate;
-        result(subNum).final_falseRate = final.falserate;
-        result(subNum).final_difference = final.difference;
-        result(subNum).fitStore = fitStore;
-        
-%         % specific to cvglmnet
-        result(subNum).lassoBestLambda = lasso.bestLambda;
-        
-%         specific to HFcvglmnet
-        result(subNum).HFsig = hit.HFsig;    
-        result(subNum).HF_tunning_lambda = HF;
-
-
-    end
-    disp('Save the results!')
-    save(['JLP_ERR_' label '.mat'], 'result');
-end
-disp('Done for all subjects and all labels!')
-
-
+% %% Run all subjects with all labels, and save the results 
+% 
+% loop over 3 labels
+% for i = 1 : 3
+% 
+%     if i == 1
+%         label = 'TrueFaces';
+%     elseif i == 2
+%         label = 'TruePlaces';
+%     elseif i == 3
+%         label = 'TrueThings';
+%     end
+% 
+%     
+%      
+%     % Loop over 10 subjects
+%     for subNum = 1:10
+%         disp(['Calculating: ' label ' | Subject: ' num2str(subNum) '...' ])
+% 
+%         %% prep
+%         [X,metadata] = loadMRIData('jlp',subNum);
+%         CVBLOCKS = metadata(subNum).CVBLOCKS;
+%         Y = metadata(subNum).(label);
+% 
+% 
+%         %% Choose the version of Iterative Lasso
+%         [ hit, final, lasso, ridge, USED, fitStore ] = IterLasso(X,Y,CVBLOCKS,2);
+% %         [ hit, final, lasso, ridge, USED, HF, fitStore ] = HFiterLasso(X,Y,CVBLOCKS,2);
+% 
+% 
+%         %% Get results, and save them
+%         result(subNum).finalAccuracy = final.accuracy;
+%         result(subNum).hitAll = hit.all;
+%         result(subNum).hitCurrent = hit.current;
+%         result(subNum).lasso_accuracy = lasso.accuracy;
+%         result(subNum).lasso_sig = lasso.sig;
+%         result(subNum).ridgeAccuracy = ridge.accuracy;
+%         result(subNum).used = USED;
+%         result(subNum).lasso_hitRate = hit.hitRate;
+%         result(subNum).lasso_falseRate = hit.falseRate;
+%         result(subNum).lasso_difference = hit.diffHF;
+%         result(subNum).lasso_difference = hit.diffHF;
+%         result(subNum).final_hitRate = final.hitrate;
+%         result(subNum).final_falseRate = final.falserate;
+%         result(subNum).final_difference = final.difference;
+%         result(subNum).fitStore = fitStore;
+%         
+% %         % specific to cvglmnet
+%         result(subNum).lassoBestLambda = lasso.bestLambda;
+%         
+% %         specific to HFcvglmnet
+%         result(subNum).HFsig = hit.HFsig;    
+%         result(subNum).HF_tunning_lambda = HF;
+% 
+% 
+%     end
+%     disp('Save the results!')
+%     save(['JLP_ERR_' label '.mat'], 'result');
+% end
+% disp('Done for all subjects and all labels!')
+% 
+% 

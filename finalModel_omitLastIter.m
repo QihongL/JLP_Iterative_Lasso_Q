cd8509304cf5d9bb40eabe016d15d1c04a1d9412
QihongL <lvqihong1992@gmail.com>
2014-11-03 12:25:22 -0600

    getting the final models (-1 iter)

--- finalModel_omitLastIter.m
+++ finalModel_omitLastIter.m
@@ -14,16 +14,20 @@ wholeBrain = load('JLP_HF_TrueFaces.mat');
 faceOnly = load('JLP_HF_onlyFace_TrueFaces.mat');
 noFace = load('JLP_HF_noFace_TrueFaces.mat');
 RESULTS = {wholeBrain.result, faceOnly.result, noFace.result};
-RESULTS_NAME = {'wholBrain', 'faceOnly', 'noFace'};
+RESULTS_NAME = {'wholeBrain', 'faceOnly', 'noFace'};
 clear wholeBrain; clear faceOnly; clear noFace;
 
 
 %% Getting voxels for the final model 
 
-i = 2;  % controlling the type (face, noface, wholebrain )
+i = 3;  % controlling the type (face, noface, wholebrain )
 
 
-for subNum = 1: 9;
+% number of subjects might not be 10, since 1 subject was completely 
+% excluded from faceOnly condition (no face voxel) 
+numberOfSubjects = size(RESULTS{i},2);
+
+for subNum = 1: numberOfSubjects;
 
     % load the metadata for one subject 
     [X,metadata] = loadMRIData('jlp',subNum);
@@ -38,30 +42,42 @@ for subNum = 1: 9;
 
     % if lasso had more than 1 iteration, only omit the last iteration
     if numIter > 1
-        targetIter = numIter - NUM_OMIT; 
+        % if all lasso iterations were significant, use all iterations
+        if numIter == sum(RESULTS{i}(subNum).HFsig)
+            targetIter = numIter;
+        else
+            targetIter = numIter - NUM_OMIT;
+        end
     else % if lasso had only 1 iteration, use it
         targetIter = 1;
     end
 
     %% Fitting the final model 
     textprogressbar(['Fitting final model: '  ])
-    for cv = 1:10
-        textprogressbar(cv * 10);
+    for cv = 1:NUM_FOLDS
+        textprogressbar(cv * NUM_FOLDS);
 
-        % get all voxels that were being used 
-        voxelUsed = RESULTS{i}(subNum).used{targetIter}(cv,:);
+        % get Indices for all voxels that were being used 
+        voxelUsedInd = RESULTS{i}(subNum).used{targetIter}(cv,:);
 
-        % check if there is any voxel in the solution
-        if sum(voxelUsed) <= 0 
-            if sum(voxelUsed) < 0 
+        % if no voxel in the solution
+        if sum(voxelUsedInd) <= 0 
+            % if no voxel, put some placeholders for the performance
+            performance.accuracy(subNum, cv) = NaN;
+            performance.hitRate(subNum, cv) = NaN;
+            performance.falseRate(subNum, cv) = NaN;
+            
+            % return some info
+            if sum(voxelUsedInd) < 0 
                 fprintf('Voxel size smaller than 0, there is something wrong! \n');
             else
-                fprintf('There is no voxel can be used to fit the final model\n');
+                fprintf('There is no voxel in this CV block. \n');
             end
+            
         else % there exist some voxels for fitting the final model 
 
             % subset the X
-            Xfinal = X(:,voxelUsed);
+            Xfinal = X(:,voxelUsedInd);
 
             % Get CV set & training set
             FINAL_HOLDOUT = CVBLOCKS(:,cv);
@@ -70,10 +86,10 @@ for subNum = 1: 9;
             Ytrain = Y(~FINAL_HOLDOUT);  
             Ytest = Y(FINAL_HOLDOUT); 
 
-            temp = CVBLOCKS(~FINAL_HOLDOUT,(1:NUM_FOLDS)~=cv); 
+            temp = CVBLOCKS(~FINAL_HOLDOUT, (1:NUM_FOLDS) ~= cv); 
             fold_id = sum(bsxfun(@times,double(temp),1:9),2);   
 
-            % Use ridge / tune lambda 
+            % tune lambda 
             opts = glmnetSet(); 
             opts.alpha = 1;   
             fitObj_cvFinal = cvglmnet (Xtrain ,Ytrain, 'binomial',opts ,...
@@ -84,6 +100,14 @@ for subNum = 1: 9;
             % Final fit using ridge
             fitObj_Final = glmnet(Xtrain, Ytrain, 'binomial', opts);
             fitTemp(subNum).CV(cv) = fitObj_Final;
+            
+            % compute predictions
+            prediction = bsxfun(@plus, Xtest * fitObj_Final.beta, fitObj_Final.a0) > 0;
+            
+            % Compute accuracy / hit rate / false alarm rate 
+            finalModels.accuracy(subNum, cv) = mean(Ytest == prediction);
+            finalModels.hitRate(subNum, cv) = sum(prediction & Ytest) / sum(Ytest);
+            finalModels.falseRate(subNum, cv) = sum(~prediction & Ytest) / sum(~Ytest);
 
         end
     end
@@ -91,7 +115,7 @@ for subNum = 1: 9;
 
 end
 
-fitStore.(RESULTS_NAME{i}) = fitTemp;
+finalModels.(RESULTS_NAME{i}) = fitTemp;
 
 % save the results
-save(['finalModels_' RESULTS_NAME{i} '.mat'], 'fitStore');
+save(['finalModels_' RESULTS_NAME{i} '.mat'], 'finalModels');
